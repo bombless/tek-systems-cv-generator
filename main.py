1849094b0f3a8f4e2b0faed6116da1c8f9d452c1
York Xiang <bombless@126.com>
2024-04-08 22:48:32 +0800

    WIP: Fill accomplishments

--- main.py
+++ main.py
@@ -24,9 +24,15 @@ if not os.path.exists('config/education.txt'):
 education_desc = io.open('config/education.txt', mode="r", encoding="utf-8")
 education = Parser(education_desc.read())
 
+if not os.path.exists('config/accomplishment.txt'):
+    raise Exception('please fill config/accomplishment.txt')
+accomplishment_desc = io.open('config/accomplishment.txt', mode="r", encoding="utf-8")
+accomplishment = Parser(accomplishment_desc.read())
+
 context = {
     'jobs': [],
     'education': [],
+    'accomplishment': [],
 }
 for section in ini.sections():
     for k in ini[section]:
@@ -46,5 +52,17 @@ while True:
         break
     context['education'].append([entry[2], entry[1]])
     context['education'].append(['', entry[0] + entry[3]])
+while True:
+    ok, entry = accomplishment.parse_item()
+    if not ok:
+        break
+    context['accomplishment'].append({
+        'time': entry[1],
+        'company': entry[2],
+        'introduction': entry[3],
+        'report_to': '',
+        'subordinates': '',
+        'responsibilities': ['foobar'],
+    })
 doc.render(context)
 doc.save("generated_doc.docx")
