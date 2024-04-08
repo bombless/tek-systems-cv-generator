267991fe2247f8055d8bc1ceb32d67e5037548d3
York Xiang <bombless@126.com>
2024-04-09 07:26:33 +0800

    Finish all informations

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ if not os.path.exists('config/education.txt'):
 education_desc = io.open('config/education.txt', mode="r", encoding="utf-8")
 education = Parser(education_desc.read())
 
-if not os.path.exists('config/accomplishment.txt'):
-    raise Exception('please fill config/accomplishment.txt')
-accomplishment_desc = io.open('config/accomplishment.txt', mode="r", encoding="utf-8")
-accomplishment = Parser(accomplishment_desc.read())
+if not os.path.exists('config/projects.txt'):
+    raise Exception('please fill config/projects.txt')
+projects_desc = io.open('config/projects.txt', mode="r", encoding="utf-8")
+projects = Parser(projects_desc.read())
 
 context = {
     'jobs': [],
     'education': [],
-    'accomplishment': [],
+    'projects': [],
 }
 for section in ini.sections():
     for k in ini[section]:
-        context[k] = ini[section][k]
+        context[k] = ini[section][k].lstrip()
 while True:
     ok, entry = jobs.parse_item()
     if not ok:
@@ -44,7 +44,13 @@ while True:
     context['jobs'].append({
         'company': entry[0],
         'time': entry[2],
-        'title': entry[1]
+        'title': entry[1],
+        'introduction': entry[3],
+        'report_to': '',
+        'subordinates': '',
+        'responsibilities': ['foo'],
+        'achievements': ['bar'],
+        'technologies': '',
     })
 while True:
     ok, entry = education.parse_item()
@@ -53,16 +59,15 @@ while True:
     context['education'].append([entry[2], entry[1]])
     context['education'].append(['', entry[0] + entry[3]])
 while True:
-    ok, entry = accomplishment.parse_item()
+    ok, entry = projects.parse_item()
     if not ok:
         break
-    context['accomplishment'].append({
+    context['projects'].append({
         'time': entry[1],
         'company': entry[2],
         'introduction': entry[3],
-        'report_to': '',
-        'subordinates': '',
-        'responsibilities': ['foobar'],
+        'responsibilities': ['foo'],
+        'achievements': ['bar'],
         'technologies': entry[4],
     })
 doc.render(context)
